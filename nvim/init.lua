3c258745efe52730f625571214860cb2825a82a7
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2026-05-16 02:04:16 +0900

    feat: 背景の設定とLSPの追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,26 @@
 -- =================================================
--- 1. Leaderキー設定 (変更なし)
+-- 1. Leaderキー設定
 -- =================================================
 vim.g.mapleader = " "
 
 -- =================================================
--- 2. lazy.nvim 初期化 (変更なし)
+-- 2. lazy.nvim 初期化
 -- =================================================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- =================================================
--- 3. 基本UI設定 (変更なし)
+-- 3. 基本UI設定
 -- =================================================
 vim.opt.number = true
 vim.opt.signcolumn = "yes"
@@ -35,15 +35,12 @@ vim.opt.clipboard = ""
 
 -- yankした時だけクリップボードに送る
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		if vim.v.event.operator == "y" then
-			vim.fn.setreg("+", vim.fn.getreg("0"))
-		end
-	end,
+    callback = function()
+        if vim.v.event.operator == "y" then
+            vim.fn.setreg("+", vim.fn.getreg("0"))
+        end
+    end,
 })
-
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -51,118 +48,144 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 -- =================================================
--- 4. プラグイン設定 (LSP関連を追加！)
+-- 4. プラグイン設定
 -- =================================================
 require("lazy").setup({
-	-- LSP管理と設定
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			-- Mason本体の起動
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "rust_analyzer" },
-			})
+    -- テーマ設定 (独立させて定義)
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                style = "storm",
+                transparent = true,
+                styles = {
+                    sidebars = "transparent",
+                    floats = "transparent",
+                },
+            })
+            vim.cmd[[colorscheme tokyonight]]
+        end,
+    },
 
-			-- [最新の書き方] vim.lsp.config を使用
-			-- これにより、警告が消え、Neovim 0.11 以降の最適化が適用されます
-			if vim.lsp.config then
-				vim.lsp.config("rust_analyzer", {
-					-- ここにサーバー固有の設定が必要なら書けます
-				})
-			else
-				-- 0.10 以前の互換性を保つためのフォールバック
-				require("lspconfig").rust_analyzer.setup({})
-			end
-		end,
-	},
+    -- LSP管理と設定
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "rust_analyzer", "clangd", "gopls" },
+            })
 
-	-- 補完エンジン (LSPの能力を最大限引き出す)
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-		},
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup({
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Enterで補完確定
-					["<Tab>"] = cmp.mapping.select_next_item(), -- Tabで次を選択
-					["<S-Tab>"] = cmp.mapping.select_prev_item(), -- Shift-Tabで前を選択
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
-			})
-		end,
-	},
+            -- LSP設定
+            if vim.lsp.config then
+                -- Neovim 0.11+ の書き方
+                vim.lsp.config("rust_analyzer", {
+                    settings = {
+                        ["rust-analyzer"] = {
+                            checkOnSave = {
+                                command = "clippy",
+                            },
+                        },
+                    },
+                })
 
-	-- fzf本体とプラグイン
-	{
-		"junegunn/fzf.vim",
-		dependencies = { "junegunn/fzf" },
-		config = function()
-			vim.g.fzf_layout = { window = { width = 0.9, height = 0.6 } }
-		end,
-	},
+                vim.lsp.config("clangd", {})
+                vim.lsp.config("gopls", {})
 
-	-- レジスタの内容を表示
-	{
-		"junegunn/vim-peekaboo",
-		config = function()
-			vim.g.peekaboo_window = "bo 20new"
-		end,
-	},
+                -- サーバーを有効化
+                vim.lsp.enable("rust_analyzer")
+                vim.lsp.enable("clangd")
+                vim.lsp.enable("gopls")
+            else
+                -- 0.10 以前の互換性用
+                local lspconfig = require("lspconfig")
+                lspconfig.rust_analyzer.setup({})
+                lspconfig.clangd.setup({})
+                lspconfig.gopls.setup({})
+            end
+        end,
+    },
 
-	{ "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{ "lewis6991/gitsigns.nvim", opts = {} },
+    -- 補完エンジン
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            })
+        end,
+    },
+
+    -- fzf
+    {
+        "junegunn/fzf.vim",
+        dependencies = { "junegunn/fzf" },
+        config = function()
+            vim.g.fzf_layout = { window = { width = 0.9, height = 0.6 } }
+        end,
+    },
+
+    -- Peekaboo
+    {
+        "junegunn/vim-peekaboo",
+        config = function()
+            vim.g.peekaboo_window = "bo 20new"
+        end,
+    },
+
+    { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "lewis6991/gitsigns.nvim", opts = {} },
 })
 
 -- =================================================
--- 5. キーバインド (LSP用アクションを追加！)
+-- 5. キーバインド (LSP用アクション)
 -- =================================================
-
--- LSPアクション (コード上での操作)
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(ev)
-		local opts = { buffer = ev.buf }
-		-- gd: 定義ジャンプ (Go Definition)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		-- K: 型情報やドキュメントを表示 (Hover)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		-- <Leader>rn: 変数名などを一括置換 (Rename)
-		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-		-- gr: その変数が使われている場所を一覧表示 (References)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-	end,
+    callback = function(ev)
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    end,
 })
 
 -- =================================================
--- 6. 自動フォーマット設定 (保存時に cargo fmt)
+-- 6. 自動フォーマット設定
 -- =================================================
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.rs",
+    pattern = { "*.rs", "*.c", "*.h", "*.go" },
     callback = function()
-        -- LSPのフォーマット機能を呼び出す
-        -- async = false にすることで、フォーマットが終わってから保存されるようになります
         vim.lsp.buf.format({ async = false })
     end,
 })
-vim.opt.fixeol = true  -- 保存時に末尾に改行がなければ足す
+vim.opt.fixeol = true
 
 -- モード切り替え
 vim.keymap.set("i", "jj", "<Esc>")
@@ -171,7 +194,7 @@ vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
 
--- ファイル検索 (fzf)
+-- ファイル検索
 vim.keymap.set("n", "<Leader>f", ":Files<CR>", { silent = true })
 
 -- ウィンドウ移動
